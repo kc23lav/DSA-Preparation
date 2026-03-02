1cb6ca330a72389c760606a63f93788c9e3eb2e4
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-03 01:15:34 +0530

    Created CountingDigits.java

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/For_Loops/CountingDigits.java
@@ -0,0 +1,22 @@
+// Counting digits of a number 
+
+import java.util.*;
+
+public class CountDigits
+{
+    public static void main(String[] args)
+     {
+        
+    int n,c=0;
+    Scanner in=new Scanner(System.in);
+
+    System.out.println("Enter the number ");
+    n=in.nextInt();
+    
+    for(int i=n;i>0;i=i/10)
+    {
+        c++;
+    }
+    System.out.println("The number of digits is : "+c);
+}
+}
