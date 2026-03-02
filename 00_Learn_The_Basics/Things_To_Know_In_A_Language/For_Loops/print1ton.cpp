cf2880b8c16aab8fed46cb0ac801507e28933b9c
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-03 01:11:59 +0530

    Created print1ton.cpp

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/For_Loops/print1ton.cpp
@@ -0,0 +1,17 @@
+// Printing numbers from 1 to N
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter the value of n "<<endl;
+    cin>>n;
+    cout<<"The sequence is : "<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+}
