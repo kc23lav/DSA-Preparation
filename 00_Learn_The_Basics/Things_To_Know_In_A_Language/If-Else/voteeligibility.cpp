e0bf80835f76bbb830c2cb6cc53bf0dfa64d466e
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-02 20:25:40 +0530

    Create voteeligibility.cpp

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/If-Else/voteeligibility.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter the age of the person "<<endl;
+    cin>>age;
+
+    if(age >= 18)
+    cout<<"The person is eligible to vote "<< endl;
+    else
+    cout<<"The person is not  eligible to vote "<< endl;
+}
