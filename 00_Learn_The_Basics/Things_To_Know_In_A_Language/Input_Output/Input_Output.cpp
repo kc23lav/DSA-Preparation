ccb8bc999ef4605ae3a3ddf3da634718a2cd4972
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-02 18:38:18 +0530

    Created Input_Output.cpp

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/Input_Output/Input_Output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int n;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter the number also: ";
+    cin >> n;
+
+    cout << "Hello " << name << endl;
+    cout << "This is the number you entered " << n << endl;
+
+    return 0;
+}
