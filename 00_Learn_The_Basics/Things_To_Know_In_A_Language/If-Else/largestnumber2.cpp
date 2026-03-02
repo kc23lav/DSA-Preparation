59df51959270daa0779899e946755847786cbe47
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-02 20:23:09 +0530

    Created largestnumber2.cpp

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/If-Else/largestnumber2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout<<"Enter the numbers to be compared "<<endl;
+    cin>>n1>>n2;
+
+    if(n1>n2)
+    cout<<"The number "<< n1 << " is greater than the number "<< n2 << endl;
+    else
+    cout<<"The number "<< n2 << " is greater than the number "<< n1 << endl;
+   
+}
