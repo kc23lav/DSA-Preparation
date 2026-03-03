f495ace8298d7bb3aa17d7f5f732158af93b3d48
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-03 12:48:49 +0530

    Create printnum.cpp

---
+++ 00_Learn_The_Basics/Things_To_Know_In_A_Language/While_loops/printnum.cpp
@@ -0,0 +1,20 @@
+//Printing numbers from 1 to N
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        
+        int N;
+        cout<< "Enter the value of N"<< endl;
+        cin>>N;
+        cout<<"The sequence is : "<<endl;
+        int i=1;
+        while(i<=N)
+        {
+            cout<<i <<endl;
+            i++;
+        }
+
+}
