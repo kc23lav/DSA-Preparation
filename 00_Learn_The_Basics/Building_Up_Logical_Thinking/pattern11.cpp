caa59b6365b117b37bc5897574900ce6b5b2acb8
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-04 15:59:02 +0530

    Created pattern11.cpp

---
+++ 00_Learn_The_Basics/Building_Up_Logical_Thinking/pattern11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        int n = 4;
+        int s=2*(n-1);
+
+        for (int i = 1; i <=n; i++)
+        {
+            for(int j=1;j<=i;j++)
+                cout<<j;
+
+            for(int j=1;j<= s;j++)
+                cout<<" ";
+
+            for(int j=i;j>=1;j--)
+                cout<<j;
+
+            cout<<endl;
+            s=s-2;
+        }
+}
