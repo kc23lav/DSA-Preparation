3d1e6859bcaa2b46e0945949694ccdb035f03ee1
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-04 16:04:06 +0530

    Created Pattern_14.java

---
+++ 00_Learn_The_Basics/Building_Up_Logical_Thinking/Pattern_14.java
@@ -0,0 +1,17 @@
+
+public class Pattern_14
+{
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=5;i++)
+        {
+            char ch='A';
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(ch);
+                ++ch;
+            }
+            System.out.println();
+        }
+    }
+}
