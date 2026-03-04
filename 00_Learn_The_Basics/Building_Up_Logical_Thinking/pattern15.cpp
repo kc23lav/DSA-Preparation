2338edf3fb586055052597461181d6603de7b114
Lavanya23 <tyagilavanya816@gmail.com>
2026-03-04 16:05:22 +0530

    Created pattern15.cpp

---
+++ 00_Learn_The_Basics/Building_Up_Logical_Thinking/pattern15.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        char ch='A';
+        for(int i=1;i<=5;i++)
+        { 
+            for(int j=i;j>=1;j--)
+            {
+                cout<<ch;
+            }
+            ch++;
+            cout<< endl;
+        }
+}
